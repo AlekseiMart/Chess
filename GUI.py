20f703344fccb92cb136ea8eefa86097197bd6bb
Aleksei Martynchik <aleksei2000@comcast.net>
2022-06-10 17:07:54 -0700

    drawing pieces

--- GUI.py
+++ GUI.py
@@ -1,6 +1,5 @@
-import tkinter as tk
-from PIL import Image, ImageTk
-
+from tkinter import *
+from PIL import Image,ImageTk
 class Piece():
     def __init__(self,color,name,location):
         self.color = color
@@ -13,17 +12,17 @@ class Piece():
 
     def draw(self):
         if self.name == 'pawn': 
-            self.photo = 'image/' + self.color + 'pawn.png'
+            self.photo = 'images/' + self.color + 'pawn.png'
         elif self.name == 'knight': 
-            self.photo = 'image/' + self.color + 'knight.png'
+            self.photo = 'images/' + self.color + 'knight.png'
         elif self.name == 'rook': 
-            self.photo = 'image/' + self.color + 'rook.png'
+            self.photo = 'images/' + self.color + 'rook.png'
         elif self.name == 'bishop': 
-            self.photo = 'image/' + self.color + 'bishop.png'
+            self.photo = 'images/' + self.color + 'bishop.png'
         elif self.name == 'queen': 
-            self.photo = 'image/' + self.color + 'queen.png'
+            self.photo = 'images/' + self.color + 'queen.png'
         elif self.name == 'king': 
-            self.photo = 'image/' + self.color + 'king.png'
+            self.photo = 'images/' + self.color + 'king.png'
         else:
             return "Error: Could not find photo"
         print(self.photo)
@@ -109,66 +108,75 @@ class Piece():
 
 
 class Chess():
-    def __init__(self, window):
-        self.window = window
+    def __init__(self):
+        self.window = Tk()
         self.checkers = {}
         self.white_pieces = []
         self.black_pieces = []
         
     def createCheckerBoard(self):
+        self.window.title("Chess")
+        self.window.resizable(width=False, height=False)
         checker_size = 90
         width = checker_size * 10
         height = checker_size * 8
-        w = tk.Canvas(self.window, width=width, height=height)
+        w = Canvas(self.window, width=width, height=height)
         for i in range(0,8):
             for j in range(0,8):
                 if (i + j) % 2  == 0:
                     w.create_rectangle(i*checker_size, j*checker_size, (i+1)*checker_size, (j+1)*checker_size, fill="tan")
                 else:
-                    w.create_rectangle(i*checker_size, j*checker_size, (i+1)*checker_size, (j+1)*checker_size, fill="black")
+                    w.create_rectangle(i*checker_size, j*checker_size, (i+1)*checker_size, (j+1)*checker_size, fill="grey")
                 self.checkers[str(i+1)+chr(j+97)] = 'NONE'
-        w.pack()
-       
-        w.create_image(50,10, image = ImageTk.PhotoImage(Image.open("wknight.png")), anchor = tk.NW)
-        w.pack()
+        self.createPieces()
+        j = 0
+        imgs = []
+        for i in self.checkers:
+            if(self.checkers[i] != 'NONE'):
+                imgs.append(ImageTk.PhotoImage(Image.open(self.checkers[i].photo).resize((80,80))))
+                x = (ord(self.checkers[i].location[1])-97)
+                y = int(self.checkers[i].location[0])-1
+                w.create_image(x*90+5,y*90+5,anchor=NW,image=imgs[j])
+                j +=1
+        w.pack()       
         w.mainloop()
 
     def createPieces(self):
-        self.white_pieces.append(Piece('w','pawn', '2a'))
-        self.white_pieces.append(Piece('w','pawn', '2b'))
-        self.white_pieces.append(Piece('w','pawn', '2c'))
-        self.white_pieces.append(Piece('w','pawn', '2d'))
-        self.white_pieces.append(Piece('w','pawn', '2e'))
-        self.white_pieces.append(Piece('w','pawn', '2f'))
-        self.white_pieces.append(Piece('w','pawn', '2g'))
-        self.white_pieces.append(Piece('w','pawn', '2h'))
+        self.white_pieces.append(Piece('b','pawn', '2a'))
+        self.white_pieces.append(Piece('b','pawn', '2b'))
+        self.white_pieces.append(Piece('b','pawn', '2c'))
+        self.white_pieces.append(Piece('b','pawn', '2d'))
+        self.white_pieces.append(Piece('b','pawn', '2e'))
+        self.white_pieces.append(Piece('b','pawn', '2f'))
+        self.white_pieces.append(Piece('b','pawn', '2g'))
+        self.white_pieces.append(Piece('b','pawn', '2h'))
 
-        self.black_pieces.append(Piece('b','pawn', '7a'))
-        self.black_pieces.append(Piece('b','pawn', '7b'))
-        self.black_pieces.append(Piece('b','pawn', '7c'))
-        self.black_pieces.append(Piece('b','pawn', '7d'))
-        self.black_pieces.append(Piece('b','pawn', '7e'))
-        self.black_pieces.append(Piece('b','pawn', '7f'))
-        self.black_pieces.append(Piece('b','pawn', '7g'))
-        #self.black_pieces.append(Piece('b','pawn', '7h'))
+        self.black_pieces.append(Piece('w','pawn', '7a'))
+        self.black_pieces.append(Piece('w','pawn', '7b'))
+        self.black_pieces.append(Piece('w','pawn', '7c'))
+        self.black_pieces.append(Piece('w','pawn', '7d'))
+        self.black_pieces.append(Piece('w','pawn', '7e'))
+        self.black_pieces.append(Piece('w','pawn', '7f'))
+        self.black_pieces.append(Piece('w','pawn', '7g'))
+        self.black_pieces.append(Piece('w','pawn', '7h'))
 
-        self.white_pieces.append(Piece('w','rook', '1a'))
-        self.white_pieces.append(Piece('w','rook', '1h'))
-        self.white_pieces.append(Piece('w','bishop', '1c'))
-        self.white_pieces.append(Piece('w','bishop', '1f'))
-        self.white_pieces.append(Piece('w','knight', '1b'))
-        self.white_pieces.append(Piece('w','knight', '1g'))
-        self.white_pieces.append(Piece('w','king', '1e'))
-        self.white_pieces.append(Piece('w','queen', '1d'))
+        self.white_pieces.append(Piece('b','rook', '1a'))
+        self.white_pieces.append(Piece('b','rook', '1h'))
+        self.white_pieces.append(Piece('b','bishop', '1c'))
+        self.white_pieces.append(Piece('b','bishop', '1f'))
+        self.white_pieces.append(Piece('b','knight', '1b'))
+        self.white_pieces.append(Piece('b','knight', '1g'))
+        self.white_pieces.append(Piece('b','king', '1e'))
+        self.white_pieces.append(Piece('b','queen', '1d'))
 
-        self.black_pieces.append(Piece('b','rook', '8a'))
-        self.black_pieces.append(Piece('b','rook', '8h'))
-        self.black_pieces.append(Piece('b','bishop', '8c'))
-        self.black_pieces.append(Piece('b','bishop', '8f'))
-        self.black_pieces.append(Piece('b','knight', '8b'))
-        self.black_pieces.append(Piece('b','knight', '8g'))
-        self.black_pieces.append(Piece('b','king', '8e'))
-        self.black_pieces.append(Piece('b','queen', '8d'))
+        self.black_pieces.append(Piece('w','rook', '8a'))
+        self.black_pieces.append(Piece('w','rook', '8h'))
+        self.black_pieces.append(Piece('w','bishop', '8c'))
+        self.black_pieces.append(Piece('w','bishop', '8f'))
+        self.black_pieces.append(Piece('w','knight', '8b'))
+        self.black_pieces.append(Piece('w','knight', '8g'))
+        self.black_pieces.append(Piece('w','king', '8e'))
+        self.black_pieces.append(Piece('w','queen', '8d'))
         for i in self.white_pieces:
             self.checkers[i.location] = i
             i.draw()
@@ -191,15 +199,9 @@ class Chess():
 
 
 def main():
-
-    window = tk.Tk()
-    window.title("Chess")
-    window.resizable(width=False, height=False)
-    chess = Chess(window)
-    chess.createPieces()
+    chess = Chess()
     chess.createCheckerBoard()
     chess.calcMoves()
-    window.mainloop()
 
 
 if __name__ == '__main__':
